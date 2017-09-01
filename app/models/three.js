a1b860bfe1838d9f27beb94fd51d45f098ab2c18
Rimian Perkins <hello@rimian.com.au>
2017-09-01 13:54:49 +1000

    set up has many relationships

--- app/models/three.js
+++ app/models/three.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  two: DS.belongsTo('two')
 });
--- app/models/two.js
+++ app/models/two.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  threes: DS.hasMany('threes'),
+  one: DS.belongsTo('one')
 });
