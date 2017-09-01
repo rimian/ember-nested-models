abc37ac977e3bf806456c22775d288207c5d12d2
Rimian Perkins <hello@rimian.com.au>
2017-09-01 14:39:11 +1000

    extend pouch models

--- app/models/two.js
+++ app/models/two.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
+import { Model } from 'ember-pouch';
 
-export default DS.Model.extend({
+export default Model.extend({
   threes: DS.hasMany('threes'),
   one: DS.belongsTo('one')
 });
