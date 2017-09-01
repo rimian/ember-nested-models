40dce285835c528c8ca2246b4f74473b6136260b
Rimian Perkins <hello@rimian.com.au>
2017-09-01 14:41:09 +1000

    put the model hook in the router

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 const { error, warn } = Ember.Logger;
 
 export default Ember.Route.extend({
+  model() {
+    warn('model hook happens');
+    return this.get('store').findAll('one');
+  },
+
   actions: {
     createModels() {
       let store = this.get('store');
